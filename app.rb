fd95f962382a3fae2ec4b3b73accbe102e0ba17f
Adam Fudge <coolfudge@gmail.com>
2017-11-18 18:36:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "Started my server using Shotgun!"
+  end
+  
+  get '/hometown' do
+    "Started my server using Shotgun!"
+  end
+  
+  get '/favorite-song' do
+    "Started my server using Shotgun!"
+  end
 end
